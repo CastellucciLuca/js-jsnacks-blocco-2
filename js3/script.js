9a2029ffc106615d3dc0777f91aeec2f962592b7
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-25 16:49:49 +0100

    3rd-snack

--- js3/script.js
+++ js3/script.js
@@ -1,10 +1,13 @@
-//Calcola la somma dei primi 10 numeri di un array.
+//Calcola la somma e la media dei primi 10 numeri di un array.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let sum = 0;
+let average = 0;
 
 for (let index = 0; index < 10; index++) {
     sum += numbers[index];   
 }
 
 console.log(sum);
+average = sum / 10;
+console.log(average);
