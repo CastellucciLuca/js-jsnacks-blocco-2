0219083d5542097c6965cd74d8161e0f954605f5
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-25 17:20:00 +0100

    fix-max-number

--- js3/script.js
+++ js3/script.js
@@ -3,12 +3,15 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let sum = 0;
 let average = 0;
+let maxNumber = 0;
 
 for (let index = 0; index < 10; index++) {
     sum += numbers[index];   
+} if (maxNumber = numbers) {
+    maxNumber = numbers;
 }
 
 console.log(sum);
 average = sum / 10;
 console.log(average);
-console.log(Math.max(...numbers));
+console.log(maxNumber);
