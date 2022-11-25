6f0ac28efa1fcc98334a8aff358e786603ff58b1
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-25 16:04:52 +0100

    2nd-snack

---
+++ js2/script.js
@@ -0,0 +1,8 @@
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+const userNumber = parseInt(prompt("Insert Number"));
+
+if (userNumber % 2 == 0){
+    console.log (userNumber);
+} else {
+    console.log (userNumber + 1)
+}
