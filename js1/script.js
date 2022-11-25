0d70814812be500ffdc8014bba6b4cc99aea3d68
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-25 15:38:22 +0100

    snack-1

--- js1/script.js
+++ js1/script.js
@@ -1,3 +1,13 @@
 //Crea un array vuoto.
 //Chiedi per 6 volte all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
+const number = [];
+
+let userNumber;
+
+for (let i = 0 ; i < 6 ; i++){
+    userNumber = parseInt(prompt("Insert Number"));
+    if (userNumber % 2 == 1){
+        number.push(userNumber);
+    }
+}
