b73a8545b70828730e44e40f4b0f702fb4eccd05
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-25 15:45:27 +0100

    plurale

--- js1/script.js
+++ js1/script.js
@@ -1,14 +1,14 @@
 //Crea un array vuoto.
 //Chiedi per 6 volte all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
-const number = [];
+const numbers = [];
 
-let userNumber;
+let userNumbers;
 
 for (let i = 0 ; i < 6 ; i++){
-    userNumber = parseInt(prompt("Insert Number"));
-    if (userNumber % 2 == 1){
-        number.push(userNumber);
+    userNumbers = parseInt(prompt("Insert Number"));
+    if (userNumbers % 2 == 1){
+        number.push(userNumbers);
     }
 }
-console.log(number);
+console.log(numbers);
